d91f2232feec2b3612e4b815c3da4a0f8162a4cf
Enrique Rodriguez Medina <erodrmed@everis.com>
2018-10-01 17:51:04 +0200

    Fixed mistakes on paths

--- first-project-app/src/main/java/com/everis/firstproject/brand/boundaries/BrandResource.java
+++ first-project-app/src/main/java/com/everis/firstproject/brand/boundaries/BrandResource.java
@@ -14,6 +14,7 @@ import javax.ws.rs.core.Response.Status;
 import com.everis.firstproject.brand.entity.Brand;
 import com.everis.firstproject.car.exceptions.CarNotFoundException;
 
+@Path("/brand")
 public class BrandResource {
 
 	@Inject
--- first-project-app/src/main/java/com/everis/firstproject/brand/entity/Brand.java
+++ first-project-app/src/main/java/com/everis/firstproject/brand/entity/Brand.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class Brand {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="ID")
 	private long id;
 	@Column(name="NAME", nullable = false)
